7d6df6d45b631eeed12b51ed11d699e184d870d6
gztanlin <gztanlin@github.com>
2026-05-22 11:58:41 +0800

    网页标题改为专属聊天室

--- server.js
+++ server.js
@@ -358,7 +358,7 @@ export default {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>💕 致我最爱的人 - 520</title>
+    <title>💕 专属聊天室</title>
     <style>
         * { margin: 0; padding: 0; box-sizing: border-box; }
         body {
